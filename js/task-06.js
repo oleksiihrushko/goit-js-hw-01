20ed873e41b26d061800d7314f37d2c448693eef
hrushkooleksii <hrushkooleksii@gmail.com>
2020-04-15 13:55:26 +0300

    123

--- js/task-06.js
+++ js/task-06.js
@@ -2,9 +2,8 @@ let input;
 let total = 0;
 do {
   input = prompt('Введите число');
-  console.log(Number.isNaN(input));
   Number.isNaN(Number(input))
-    ? alert('Было введено не число, попробуйте еще раз')
+    ? alert('Было введено не число, попробуйте еще раз!')
     : (total += Number(input));
 } while (input !== null);
 
